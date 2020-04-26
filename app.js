0c175c657d1b95e7ee76da8744906bf56a6c0642
crimson225 <crimson225@gmail.com>
2020-04-26 00:57:10 -0400

    fixed point labels and color change. Still need text on axis and under graph

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var margin = {top: 10, right: 100, bottom: 30, left: 30},
-    width = 600 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+var margin = {top: 5, right: 100, bottom: 30, left: 30},
+    width = 650 - margin.left - margin.right,
+    height = 550 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
@@ -14,10 +14,10 @@ var svg = d3.select("#my_dataviz")
 //Read the data
 d3.csv("JoinedData.csv", function(data) {
 
-    // List of groups (here I have one group per column)
+    // List of groups 
     var allGroup = ["2018", "2017", "2016", "2015", "2014","2013","2012","2011","2010","2009","2008","2007","2006","2005","2004","2003","2002","2001","2000"]
 
-        // Initialize dots with 2000
+        // Initialize dots with 2018
     var renderDots = function(data,year) {
         return svg
       .selectAll('circle')
@@ -29,14 +29,14 @@ d3.csv("JoinedData.csv", function(data) {
         .attr("r", 5)
         .style("fill", "blue");
     }
+    
     var renderLabels = function(data,year) {
-        return svg.selectAll("text")
+        return svg.selectAll("div.label")
         .data(data)
         .enter()
         .append("text")
-      //   .text(function(d) {
-      //     return d[0] + "," + d[1];})
-          .text(function(d){return d.State;})
+          .text("")
+          .text(function(d){ return d.State})
           .attr("x", function(d) {
             return x(d[year]);})//location X
           .attr("y", function(d) {
@@ -45,6 +45,7 @@ d3.csv("JoinedData.csv", function(data) {
           .attr("font_size","10px")
           .attr("fill","black");
     }
+  
     // add the options to the button
     d3.select("#selectButton")
       .selectAll('myOptions')
@@ -66,17 +67,53 @@ d3.csv("JoinedData.csv", function(data) {
     var y = d3.scaleLinear()
       .domain( [0,8])
       .range([ height, 0 ]);
-    svg.append("g")
+    var yaxis = svg.append("g")
       .call(d3.axisLeft(y));
      // .text("Unemployment Rate");
 
     //render dots
-    var dot = renderDots(data,2000);
+    var dot = renderDots(data,2018);
     // Add labels
-    
-    var labels = renderLabels(data,2000);
-        
-    d3.select("#selectButton").on("change", function(d) {
+    var labels = renderLabels(data,2018);
+    var granimInstance = new Granim({
+      element: '#canvas-image-blending',
+      direction: 'top-bottom',
+      isPausedWhenNotInView: true,
+      defaultStateName: "low-unemploy",
+      // image : {
+      //     source: './floortraders.jpg',
+      //     blendingMode: 'multiply'
+      // },
+      states : {
+        //Greens for Low
+        "low-unemploy": {
+            gradients: [
+                ['#B3FFAB', '#26D0CE'],
+                ['#198C19', '#ADFF00']
+            ],
+            transitionSpeed: 10000
+        },
+        //Blues for Med
+        "med-unemploy": {
+            gradients: [
+                ['#9D50BB', '#6E48AA'],
+                ['#4776E6', '#8E54E9']
+            ],
+            transitionSpeed: 2000
+        },
+        //Reds and yellows for High
+        "high-unemploy": {
+            gradients: [ 
+              ['#FF4E50', '#F9D423'],
+              ['#FF0000', '#FF8000']
+             ],
+            transitionSpeed: 2000,
+            loop: false
+        }
+      }
+  });    
+  
+  d3.select("#selectButton").on("change", function(d) {
                         // recover the option that has been chosen
                     var selectedOption = d3.select(this).property("value")
                         // run the updateChart function with this selected option
@@ -85,25 +122,51 @@ d3.csv("JoinedData.csv", function(data) {
     // A function that update the chart
     function update(selectedOption) {
        
-        renderDots(data,selectedOption)
-        renderLabels(data,selectedOption)
+        //renderDots(data,selectedOption)
+        //renderLabels(data,selectedOption)
 
       // Give these new data to update line
+
+
+
+      var adjustedY = Math.max(...data.map(x => parseFloat(x[`Rate${selectedOption}`])))
+      y
+        .domain([0,adjustedY+1])
+      yaxis.transition()
+        .duration(1000)
+        .call(d3.axisLeft(y));
+
       labels
         .data(data)
         .transition()
         .duration(1000)
           .attr("x", function(d) { return x(+d[selectedOption]) })
-          .attr("y", function(d) { return y(+d[`Rate${selectedOption}`]) })
+          .attr("y", function(d) { return y(+d[`Rate${selectedOption}`]) });
+
       dot
         .data(data)
         .transition()
         .duration(1000)
           .attr("cx", function(d) { return x(+d[selectedOption]) })
-          .attr("cy", function(d) { return y(+d[`Rate${selectedOption}`]) })
+          .attr("cy", function(d) { return y(+d[`Rate${selectedOption}`]) });
+
+      if (adjustedY <=7) {
+        var changedGradient = "low-unemploy";
+      }
+      else if (adjustedY >= 7.1 && adjustedY <= 8.9) {
+        var changedGradient = "med-unemploy";
+      }
+      else if (adjustedY >9) { 
+        var changedGradient = "high-unemploy";
+      }
+    
+
+      granimInstance.changeState(changedGradient)  
+      console.log(selectedOption)
+      console.log(changedGradient)   
+      console.log(adjustedY)
     }
 
     // When the button is changed, run the updateChart function
    
-
-})
+  });
